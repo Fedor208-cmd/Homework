1753c1e998b357d8fb5ce4e2723d71fcc818cfca
Fedor208-cmd <fedorfalev@gmail.com>
2025-04-13 15:25:22 +0300

    game_car_with_bomb.py

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import tkinter
+import time
 
 master = tkinter.Tk()
 canvas = tkinter.Canvas(master, bg="light green", width=1000, height=800)
+
 
 def handler(event):
     if canvas.coords(car)[0] < 0:
@@ -29,13 +31,13 @@ def handler(event):
     elif event.char == "m":
         canvas.moveto(car, event.x, event.y)
     if event.char == "f":
-        x1 = canvas.coords(car)[2]
-        y1 = canvas.coords(car)[1]
-        x2 = x1 + 30
-        y2 = y1 + 30
-        pr = canvas.create_oval((x1, y1), (x2, y2), fill="black")
-        while canvas.coords(pr)[2] < 1000:
-            canvas.move(pr, 10, 0)
+        bomb = canvas.create_oval((canvas.coords(car)[2] - 5, canvas.coords(car)[1]),
+                                  (canvas.coords(car)[2] + 25, canvas.coords(car)[1] + 30), fill="black")
+        while canvas.coords(bomb)[0] < canvas.winfo_reqwidth():
+            time.sleep(0.03)
+            canvas.move(bomb, 10, 0)
+            canvas.update()
+
 
 car = canvas.create_rectangle((200, 150), (250, 180), fill="purple")
 master.bind("<KeyPress>", handler)
