254a7220b37ac90d6298fb4843dc5f9b969cba7b
Fedor208-cmd <fedorfalev@gmail.com>
2025-04-21 15:31:16 +0300

    game_with_working_wall_and_a_goal.py

--- game.py
+++ game.py
@@ -5,8 +5,26 @@ master = tkinter.Tk()
 canvas = tkinter.Canvas(master, bg="light green", width=1000, height=800)
 
 
-def handler(event):
+def collision(obj, obst=0):
+    xo1, yo1, xo2, yo2 = canvas.coords(obj)
+    for wall in walls:
+        xw1, yw1, xw2, yw2 = canvas.coords(wall)
+        if (xw1 < xo1 < xw2 or xw1 < xo2 < xw2) and (yw1 < yo1 < yw2 or yw1 < yo2 < yw2):
+            return True
+    return False
 
+
+def collision_goal(obj, goal):
+    if canvas.coords(obj)[2] >= canvas.coords(goal)[0] and (
+            canvas.coords(goal)[1] < canvas.coords(obj)[1] <
+            canvas.coords(goal)[3] or canvas.coords(goal)[1] < canvas.coords(obj)[2] <
+            canvas.coords(goal)[3]
+    ):
+        return True
+    return False
+
+
+def handler(event):
     if canvas.coords(car)[0] < 0:
         canvas.moveto(car, 949)
     if canvas.coords(car)[1] < 0:
@@ -15,27 +33,22 @@ def handler(event):
         canvas.moveto(car, 0)
     if canvas.coords(car)[3] > 800:
         canvas.moveto(car, y=0)
-    if event.keycode == 39 and not (
-        canvas.coords(wall)[0] <= canvas.coords(car)[2] <= canvas.coords(wall)[2] and
-        (canvas.coords(wall)[1] <= canvas.coords(car)[1] <= canvas.coords(wall)[3] or
-         canvas.coords(wall)[1] <= canvas.coords(car)[3] <= canvas.coords(wall)[3])
-    ):
+    if event.keycode == 39:
         canvas.move(car, 10, 0)
-    elif event.keycode == 38 and not (canvas.coords(wall)[1] <= canvas.coords(car)[1] <= canvas.coords(wall)[3] and (
-            canvas.coords(wall)[0] <= canvas.coords(car)[2] <= canvas.coords(wall)[2] or
-            canvas.coords(wall)[0] <= canvas.coords(car)[0] <= canvas.coords(wall)[2]
-    )):
+        if collision(car):
+            canvas.move(car, -10, 0)
+    elif event.keycode == 38:
         canvas.move(car, 0, -10)
-    elif event.keycode == 40 and not (canvas.coords(wall)[1] <= canvas.coords(car)[3] <= canvas.coords(wall)[3] and (
-            canvas.coords(wall)[0] <= canvas.coords(car)[2] <= canvas.coords(wall)[2] or
-            canvas.coords(wall)[0] <= canvas.coords(car)[0] <= canvas.coords(wall)[2]
-    )):
+        if collision(car):
+            canvas.move(car, 0, 10)
+    elif event.keycode == 40:
         canvas.move(car, 0, 10)
-    elif event.keycode == 37 and not (canvas.coords(wall)[0] <= canvas.coords(car)[0] <= canvas.coords(wall)[2] and (
-            (canvas.coords(wall)[1] <= canvas.coords(car)[1] <= canvas.coords(wall)[3] or
-             canvas.coords(wall)[1] <= canvas.coords(car)[3] <= canvas.coords(wall)[3])
-    )):
+        if collision(car):
+            canvas.move(car, 0, -10)
+    elif event.keycode == 37:
         canvas.move(car, -10, 0)
+        if collision(car):
+            canvas.move(car, 10, 0)
     elif event.char == "b":
         canvas.itemconfig(car, fill="cyan")
     elif event.char == "y":
@@ -47,26 +60,22 @@ def handler(event):
     if event.char == "f":
         bomb = canvas.create_oval((canvas.coords(car)[2] - 5, canvas.coords(car)[1]),
                                   (canvas.coords(car)[2] + 25, canvas.coords(car)[1] + 30), fill="black")
-        while canvas.coords(bomb)[0] < canvas.winfo_reqwidth() and not (
-         canvas.coords(wall)[0] <= canvas.coords(bomb)[2] <= canvas.coords(wall)[2] and (
-                canvas.coords(wall)[1] <= canvas.coords(bomb)[1] <= canvas.coords(wall)[3] or
-                canvas.coords(wall)[1] <= canvas.coords(bomb)[3] <= canvas.coords(wall)[3])
-        ):
+        while canvas.coords(bomb)[0] < canvas.winfo_reqwidth() and not collision((bomb)) and not \
+                collision_goal(bomb, goal):
             time.sleep(0.03)
             canvas.move(bomb, 10, 0)
             canvas.update()
-        if canvas.coords(bomb)[0] > canvas.coords(goal)[0] and (
-         canvas.coords(goal)[1] < canvas.coords(bomb)[1] <
-         canvas.coords(goal)[3] or canvas.coords(goal)[1] < canvas.coords(bomb)[2] <
-         canvas.coords(goal)[3]
-         ):
+        if collision_goal(bomb, goal):
             canvas.itemconfig(goal, fill="green")
+        canvas.delete(bomb)
 
 
+
+walls = list()
 goal = canvas.create_rectangle((900, 200), (1000, 600), fill="yellow")
-wall = canvas.create_rectangle((400, 200), (450, 300), fill="pink")
+wall = canvas.create_rectangle((400, 200), (550, 300), fill="pink")
 car = canvas.create_rectangle((200, 150), (250, 180), fill="purple")
 master.bind("<KeyPress>", handler)
-
+walls.append(wall)
 canvas.pack()
 master.mainloop()
