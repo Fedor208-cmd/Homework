5517f5c288922c0ea65f885b7d97ccd13bc23a07
Fedor208-cmd <fedorfalev@gmail.com>
2025-05-04 18:07:14 +0300

    Update game.py

--- game.py
+++ game.py
@@ -79,3 +79,136 @@ master.bind("<KeyPress>", handler)
 walls.append(wall)
 canvas.pack()
 master.mainloop()
+
+
+
+import tkinter
+import time
+import random
+
+w = 1000
+h = 600
+shift_x = w // 5
+shift_y = h // 5
+
+master = tkinter.Tk()
+canvas = tkinter.Canvas(master, bg="light blue", width=w, height=h)
+
+suns = 500
+peashooters = list()
+sunflowers = list()
+used = list()
+zombies = list()
+
+
+def collision(event, button):
+    xo1, yo1 = event.x, event.y
+    xb1, xb2, yb1, yb2 = canvas.coords(button)[0], canvas.coords(button)[2], canvas.coords(button)[1], canvas.coords(button)[3]
+    if (xb1 < xo1 < xb2) and (yb1 < yo1 < yb2):
+        return True
+    return False
+
+
+def sunflower_draw(xs, ys):
+    coord = canvas.create_oval((xs, ys), (xs + w // 10, ys + h // 5), outline="orange", fill="yellow")
+    canvas.create_oval((xs + 20, ys + 20), (xs + w // 10 - 20, ys + h // 5 - 20), outline="#542626", fill="#6E3131")
+    return coord
+
+
+def peashooter_draw(xp, yp):
+    coord = canvas.create_rectangle((xp, yp), (xp + w // 10, yp + h // 5), fill="green", outline="black")
+    canvas.create_rectangle((xp + w // 15, yp + 30), (xp + w // 10 - 2, yp + h // 5 - 30), fill="black")
+    return coord
+
+
+def zombie(x_l, y_l):
+    global zombies
+    zombie_mob = canvas.create_oval((x_l, y_l), (x_l + w // 10, y_l + w // 10), fill="#008000", outline="#808000")
+    zombies.append(zombie_mob)
+    while canvas.coords(zombie_mob)[0] < 0:
+        time.sleep(0.05)
+        canvas.move(zombie_mob, -10, 0)
+        canvas.update()
+
+
+plant = None
+
+
+def handler(event):
+    global used
+    global plant
+    global suns
+    global sunflowers
+
+    if collision(event, button_sunflower):
+        plant = "Sunflower"
+    if collision(event, button_peashooter):
+        plant = "Peashooter"
+    for pot in field:
+        if collision(event, pot) and plant == "Sunflower" and pot not in used and suns >= 500:
+            sunflowers.append(sunflower_draw(canvas.coords(pot)[0], canvas.coords(pot)[1]))
+            used.append(pot)
+            canvas.update()
+            plant = None
+            suns -= 500
+            break
+        if collision(event, pot) and plant == "Peashooter" and pot not in used and suns >= 1000:
+            peashooters.append(peashooter_draw(canvas.coords(pot)[0], canvas.coords(pot)[1]))
+            used.append(pot)
+            canvas.update()
+            plant = None
+            suns -= 1000
+            break
+
+    for s_fl in sunflowers:
+        if collision(event, s_fl):
+            suns += 100 * len(sunflowers)
+            amount.config(text=f"Солнца: {suns}")
+            amount.pack()
+    for p_sh in peashooters:
+        if collision(event, p_sh):
+            yp1, xp2, yp2 = canvas.coords(p_sh)[1], canvas.coords(p_sh)[2], canvas.coords(p_sh)[3]
+            bomb = canvas.create_oval((xp2, yp1), (xp2 + yp2, yp2), fill="black")
+            while canvas.coords(bomb)[2] < 0:
+                time.sleep(0.05)
+                canvas.move(bomb, 10, 0)
+                canvas.update()
+
+
+amount = tkinter.Label(text=f"Солнца: {suns}")
+amount.pack()
+field = list()
+colour = "green"
+button_sunflower = canvas.create_rectangle((0, shift_y), (shift_x, (shift_y * 2)), fill="#D2B48C", outline="#8B4513")
+
+xsa = canvas.coords(button_sunflower)[0]
+ysa = canvas.coords(button_sunflower)[1]
+canvas.create_text(xsa + 150, ysa * 1.5, font=("Masquerade", 12), text="500")
+canvas.create_oval((xsa, ysa), (xsa + w // 10, ysa + h // 5), outline="orange", fill="yellow")
+canvas.create_oval((xsa + 20, ysa + 20), (xsa + w // 10 - 20, ysa + h // 5 - 20), outline="#542626", fill="#6E3131")
+
+button_peashooter = canvas.create_rectangle((0, shift_y * 2), (shift_x, (shift_y * 3)), fill="#D2B48C", outline="#8B4513")
+xpa = canvas.coords(button_peashooter)[0]
+ypa = canvas.coords(button_peashooter)[1]
+canvas.create_text(xpa + 150, ypa * 1.25, font=("Masquerade", 12), text="1000")
+canvas.create_rectangle((xpa, ypa), (xpa + w // 10, ypa + h // 5), fill="green", outline="black")
+canvas.create_rectangle((xpa + w // 15, ypa + 30), (xpa + w // 10 - 2, ypa + h // 5 - 30), fill="black")
+
+for y in range(0, h, h // 5):
+    if colour == "green":
+        colour = "dark green"
+    elif colour == "dark green":
+        colour = "green"
+    for x in range(shift_x, w, (w - shift_x) // 8):
+        if colour == "green":
+            square = canvas.create_rectangle((x, y), (x + 100, y + 120), fill=colour)
+            colour = "dark green"
+        else:
+            square = canvas.create_rectangle((x, y), (x + 100, y + 120), fill=colour)
+            colour = "green"
+        field.append(square)
+
+master.bind("<ButtonPress>", handler)
+canvas.pack()
+master.mainloop()
+
